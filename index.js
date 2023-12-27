8bb69e98300f5372429667365741b3a8d94f27c4
vijaynvvr <vijayvardhansn10@gmail.com>
2023-12-27 23:31:10 +0530

    adding cors origin

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({origin:process.env.CLIENT_URL, credentials:true}))
-app.use(cors({credentials:true}));
+app.use(cors({origin:process.env.CLIENT_URL, credentials:true}))
+// app.use(cors({credentials:true}));
 
 const PORT = process.env.PORT || 5000;
 
